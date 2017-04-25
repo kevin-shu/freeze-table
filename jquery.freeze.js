3c447ed75e6e42a894a21b9f92e91e6332acdf14
Evan Jehu <evan.jehu@gmail.com>
2017-04-25 15:44:25 -0400

    Make horizontal scrollbars appear when required
    always hide vertical scrollbars, and fix a few IntelliJ flagged JS issues.

--- jquery.freeze.js
+++ jquery.freeze.js
@@ -1,14 +1,14 @@
 $.fn.freeze=function(colNum, rowNum){
     this.each( function(){
-        $this = $(this);
+        var $this = $(this);
         colNum = colNum || 1;
         rowNum = rowNum || 0;
 
-        var $wrapper = $("<div style='position:relative;'></div>");
+        var $wrapper = $("<div style='position:relative;overflow-y:hidden'></div>");
         var $fixedColumn = $this.clone().removeAttr("id").css("position","absolute");
         $wrapper.insertBefore($this);
         $wrapper.append($fixedColumn);
-        $wrapper.append($("<div style='overflow:scroll;'></div>").append($this));
+        $wrapper.append($("<div style='overflow-x:auto;'></div>").append($this));
 
         $fixedColumn.find("tr").each(function(i,elem){
             $(elem).children().remove();
@@ -16,7 +16,7 @@ $.fn.freeze=function(colNum, rowNum){
         var wrapperWidth = 0;
 
         $this.find("tr").each(function(i,row){
-            var $fixedRow = $($fixedColumn.find("tr")[i])
+            var $fixedRow = $($fixedColumn.find("tr")[i]);
             if(i<rowNum){
 
             } else {
@@ -24,10 +24,10 @@ $.fn.freeze=function(colNum, rowNum){
                     var $col = $(row.children[j]);
                     var $newCol = $(row.children[j].cloneNode(true));
                     $fixedRow.append($newCol);
-                    if(j==0){
+                    if(j===0){
                         $newCol.outerHeight($col.outerHeight());
                     }
-                    if(i==0){
+                    if(i===0){
                         $newCol.outerWidth($col.outerWidth());
                         wrapperWidth += $col.outerWidth();
                     }
@@ -37,4 +37,4 @@ $.fn.freeze=function(colNum, rowNum){
         var pos = $this.position();
         $fixedColumn.css("width",wrapperWidth).css("top",pos.top).css("left",pos.left);
     });
-}
+};
